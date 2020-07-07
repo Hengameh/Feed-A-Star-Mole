f15971a76d55eb520641b06106bdf34875bd8253
Henny Habibpour <he.habibpour@gmail.com>
2020-07-06 22:06:41 -0700

    Hungry status

--- game.js
+++ game.js
@@ -93,7 +93,12 @@ function getNextStatus (mole) {
             mole.node.children[0].classList.add("hungry");
             mole.node.children[0].src = './images/mole-hungry.png';
             break;
-        
+        case "hungry":
+            mole.status = 'sad';
+            mole.next = getSadInterval();
+            mole.node.children[0].classList.remove("hungry");
+            mole.node.children[0].src = './images/mole-sad.png';
+            break;
     }
 }
 
